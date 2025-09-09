fbf4c992cb6b49bb8b530278801560959d9ea9bc
iPranavD <163492169+iPranavD@users.noreply.github.com>
2025-09-08 20:02:36 -0700

    hi

--- .d/main.cpp.d
+++ .d/main.cpp.d
@@ -665,8 +665,8 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/pros/motors.hpp include/pros/optical.hpp include/pros/optical.h \
  include/pros/rotation.hpp include/pros/rotation.h include/pros/rtos.hpp \
  include/pros/screen.hpp include/pros/screen.h include/pros/vision.hpp \
- include/pros/vision.h src/config.hpp include/lemlib/api.hpp \
- include/lemlib/pid.hpp include/lemlib/pose.hpp include/lemlib/util.hpp \
+ include/pros/vision.h include/lemlib/api.hpp include/lemlib/pid.hpp \
+ include/lemlib/pose.hpp include/lemlib/util.hpp \
  include/lemlib/chassis/chassis.hpp include/pros/imu.hpp \
  include/lemlib/asset.hpp include/lemlib/chassis/trackingWheel.hpp \
  include/pros/motor_group.hpp include/pros/adi.hpp \
@@ -676,7 +676,7 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/fmt/format-inl.h include/lemlib/logger/baseSink.hpp \
  include/fmt/args.h include/lemlib/logger/message.hpp \
  include/lemlib/logger/infoSink.hpp \
- include/lemlib/logger/telemetrySink.hpp src/colorsort.hpp src/autons.hpp
+ include/lemlib/logger/telemetrySink.hpp
 include/main.h:
 include/api.h:
 include/pros/adi.h:
@@ -1415,7 +1415,6 @@ include/pros/screen.hpp:
 include/pros/screen.h:
 include/pros/vision.hpp:
 include/pros/vision.h:
-src/config.hpp:
 include/lemlib/api.hpp:
 include/lemlib/pid.hpp:
 include/lemlib/pose.hpp:
@@ -1439,5 +1438,3 @@ include/fmt/args.h:
 include/lemlib/logger/message.hpp:
 include/lemlib/logger/infoSink.hpp:
 include/lemlib/logger/telemetrySink.hpp:
-src/colorsort.hpp:
-src/autons.hpp:
